d133320ac00e259a70c24b5d99fcf39583179dec
Frank-Wiebeler <frank.wiebeler@dev4Agriculture.de>
2021-05-22 09:52:32 +0200

    -FIX: Removed Typo from build_apps.sh (was not commited last time

--- build_apps.sh
+++ build_apps.sh
@@ -3,4 +3,4 @@ mvn clean install
 mkdir apps
 cp cli/target/cli-1.4-SNAPSHOT.jar ./apps/ar_telemetry_converter.jar
 cp ./scripts/ar_telemetry_converter.bat ./apps/ar_telemetry_converter.bat
-cp ./scripts/ar_telemetry_converter.sh ./apps/ar_telemetry_converter.shbuild_apps.sh
+cp ./scripts/ar_telemetry_converter.sh ./apps/ar_telemetry_converter.sh
